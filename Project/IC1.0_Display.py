104752674ca5adf0da278d914f4ffb6638f334df
preetam shetty <preetamshetty22@gmail.com>
2019-08-08 15:38:10 +0530

    Hotfix: Added Fuel Level indicator changes to IC1.0_Display.py

--- Project/IC1.0_Display.py
+++ Project/IC1.0_Display.py
@@ -16,10 +16,13 @@ PURPLE = (128, 0, 128)
 TEAL = (0, 128, 128)
 LIME = (0, 255, 0)
 GREEN = (0, 128, 0)
+RICHGREEN = (81, 225, 115)
 OLIVE = (128, 128, 0)
 YELLOW = (255, 255, 0)
+RICHYELLOW = (211, 216, 66)
 ORANGE = (255, 165, 0)
 RED = (255, 0, 0)
+RICHRED = (229, 20, 20)
 MAROON = (128, 0, 0)
 SILVER = (192, 192, 192)
 GRAY = (128, 128, 128)
@@ -31,12 +34,16 @@ BLACK = (0, 0, 0)
 
 '''Goabls We should not use it but for experiments we still keep this'''
 degrees = 145;
+FUELX = 100;
+FUELCOLOR = GREEN;
+FUELPERCVALUE = 100;
+SELECTED = 1;
 
 pygame.init()
 CLOCK = pygame.time.Clock() # *** NOT COVERED IN THE VIDEO ***
 
 ''' DISPLAY SETUP -------------------------------------------------------------------------------- DISPLAY SETUP '''
-DISPLAY_WIDTH = 940
+DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 480
 DW_HALF = DISPLAY_WIDTH // 2
 DH_HALF = DISPLAY_HEIGHT // 2
@@ -45,36 +52,50 @@ DS = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT), pygame.FULLSCREEN)
 
 ''' LOAD IMAGES ---------------------------------------------------------------------------------- LOAD IMAGES '''
 Needle_org = pygame.image.load('./images/needle.png')
-Gauge_org = pygame.image.load('./images/CGaugeBlk.png')
+Gauge = pygame.image.load('./images/CGaugeBlk.png')
 Circle_org = pygame.image.load('./images/circle2.jpg')
 
 
-Gauge = pygame.transform.scale(Gauge_org, (640,460))
-Needle = pygame.transform.scale(Needle_org, (260,460))
+#Gauge = pygame.transform.scale(Gauge_org, (340,260))
+Needle = pygame.transform.scale(Needle_org, (260,360))
 Circle = pygame.transform.scale(Circle_org, (340, 260))
+
 ''' FUNCTIONS ------------------------------------------------------------------------------------ FUNCTIONS '''
 def event_handler():
         global degrees
+        global FUELX
+        global FUELPERCVALUE
+        
         for event in pygame.event.get():
                 if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                         pygame.quit()
                         sys.exit()
                         
-                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_DOWN):
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_LEFT):
                         #global degrees
                         degrees += 1
                         if degrees > 145:
                                 degrees = 145
 
-                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_UP):
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_RIGHT):
                         #global degrees
                         degrees -= 1
                         if degrees < -145:
                                 degrees = -145
 
-			
+                if (event.type == pygame.KEYDOWN and event.key == pygame.K_4):
+                        if(FUELX > 0):
+                                FUELX -= 10
+                                FUELPERCVALUE -= 10
+
+                if (event.type == pygame.KEYDOWN and event.key == pygame.K_6):
+                        if(FUELX < 100):
+                                FUELX += 10
+                                FUELPERCVALUE += 10
+                                
 def rotate_and_center(ds, x, y, image, degrees):
 # this function rotates an image and then centralises so that the rotation is uniform
+
 	# rotate the object n degrees and store in a new surface called 'rotated'
         rotated = pygame.transform.rotate(image, degrees)
 	
@@ -99,6 +120,7 @@ def display_text(text, x_cord, y_cord, size):
         textRect = textSurf.get_rect()
         textRect.center = (x_cord, y_cord)
         DS.blit(textSurf, textRect)
+        return textSurf
 
 def display_text_NC(text, x_cord, y_cord, size):
         texttype = pygame.font.Font('Light.ttf', size)
@@ -111,8 +133,8 @@ def display_text_NC(text, x_cord, y_cord, size):
 # set the starting rotation of the image in degrees 
 direction = 1;
 raw_value = degrees * 0.8
-MenuStart_X = DW_HALF + 100
-MenuStart_Y = 280
+MenuStart_X = DW_HALF + 80
+MenuStart_Y = 260
 Y_MenuSpace = 25
 
 ''' MAIN LOOP ------------------------------------------------------------------------------------ MAIN LOOP '''
@@ -122,9 +144,17 @@ while True:
 
         #Place the Gauge first (and any other fixed shapes)
         RECT = Gauge.get_rect()        
-        DS.blit(Gauge, (DW_HALF - RECT.center[0] - 230, DH_HALF - RECT.center[1]))
-        pygame.draw.rect(DS, GREEN, (DW_HALF + 190, 173, 100, 30), 0)
-        pygame.draw.rect(DS, SILVER, (DW_HALF + 85, 255, 280, 220), 5)
+        DS.blit(Gauge, (DW_HALF - RECT.center[0] - 170, DH_HALF - RECT.center[1]))
+        pygame.draw.rect(DS, WHITE, (DW_HALF + 190, 173, 110, 30), 1)
+        if(FUELPERCVALUE > 60):
+                FUELCOLOR = RICHGREEN
+        elif(FUELPERCVALUE < 60):
+                if(FUELPERCVALUE > 30):
+                        FUELCOLOR = RICHYELLOW
+                else:
+                        FUELCOLOR = RICHRED
+        pygame.draw.rect(DS, FUELCOLOR, (DW_HALF + 195, 178, FUELX, 20), 0) 
+        pygame.draw.rect(DS, SILVER, (DW_HALF + 65, 235, 280, 220), 1)
 
         #Place all the fixed texts here
         display_text('RPM', DW_HALF+100, 50, 30)
@@ -135,7 +165,7 @@ while True:
         
 
         #and then show the changed position of the needle
-        rotate_and_center(DS, DW_HALF - 230, DH_HALF, Needle, degrees)
+        rotate_and_center(DS, DW_HALF - 170, DH_HALF, Needle, degrees)
 
 	#The degrees as of now is the global variable 
         raw_value = float(((145 - degrees) * 28)/1000)
