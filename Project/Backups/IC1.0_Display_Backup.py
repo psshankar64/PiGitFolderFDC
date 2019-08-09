855c1e1235eb254278d2a914c9f9fb1fce2817c9
Shankar <psshankar64@yahoo.com>
2019-08-09 12:34:15 +0530

    Merged changes for display sizes from initial version

--- Project/Backups/IC1.0_Display_Backup.py
+++ Project/Backups/IC1.0_Display_Backup.py
@@ -16,13 +16,10 @@ PURPLE = (128, 0, 128)
 TEAL = (0, 128, 128)
 LIME = (0, 255, 0)
 GREEN = (0, 128, 0)
-RICHGREEN = (81, 225, 115)
 OLIVE = (128, 128, 0)
 YELLOW = (255, 255, 0)
-RICHYELLOW = (211, 216, 66)
 ORANGE = (255, 165, 0)
 RED = (255, 0, 0)
-RICHRED = (229, 20, 20)
 MAROON = (128, 0, 0)
 SILVER = (192, 192, 192)
 GRAY = (128, 128, 128)
@@ -34,10 +31,6 @@ BLACK = (0, 0, 0)
 
 '''Goabls We should not use it but for experiments we still keep this'''
 degrees = 145;
-FUELX = 100;
-FUELCOLOR = GREEN;
-FUELPERCVALUE = 100;
-SELECTED = 1;
 
 pygame.init()
 CLOCK = pygame.time.Clock() # *** NOT COVERED IN THE VIDEO ***
@@ -59,43 +52,29 @@ Circle_org = pygame.image.load('./images/circle2.jpg')
 Gauge = pygame.transform.scale(Gauge_org, (640,460))
 Needle = pygame.transform.scale(Needle_org, (260,460))
 Circle = pygame.transform.scale(Circle_org, (340, 260))
-
 ''' FUNCTIONS ------------------------------------------------------------------------------------ FUNCTIONS '''
 def event_handler():
         global degrees
-        global FUELX
-        global FUELPERCVALUE
-        
         for event in pygame.event.get():
                 if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                         pygame.quit()
                         sys.exit()
                         
-                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_LEFT):
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_DOWN):
                         #global degrees
                         degrees += 1
                         if degrees > 145:
                                 degrees = 145
 
-                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_RIGHT):
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_UP):
                         #global degrees
                         degrees -= 1
                         if degrees < -145:
                                 degrees = -145
 
-                if (event.type == pygame.KEYDOWN and event.key == pygame.K_4):
-                        if(FUELX > 0):
-                                FUELX -= 10
-                                FUELPERCVALUE -= 10
-
-                if (event.type == pygame.KEYDOWN and event.key == pygame.K_6):
-                        if(FUELX < 100):
-                                FUELX += 10
-                                FUELPERCVALUE += 10
-                                
+			
 def rotate_and_center(ds, x, y, image, degrees):
 # this function rotates an image and then centralises so that the rotation is uniform
-
 	# rotate the object n degrees and store in a new surface called 'rotated'
         rotated = pygame.transform.rotate(image, degrees)
 	
@@ -120,7 +99,6 @@ def display_text(text, x_cord, y_cord, size):
         textRect = textSurf.get_rect()
         textRect.center = (x_cord, y_cord)
         DS.blit(textSurf, textRect)
-        return textSurf
 
 def display_text_NC(text, x_cord, y_cord, size):
         texttype = pygame.font.Font('Light.ttf', size)
@@ -133,8 +111,8 @@ def display_text_NC(text, x_cord, y_cord, size):
 # set the starting rotation of the image in degrees 
 direction = 1;
 raw_value = degrees * 0.8
-MenuStart_X = DW_HALF + 80
-MenuStart_Y = 260
+MenuStart_X = DW_HALF + 100
+MenuStart_Y = 280
 Y_MenuSpace = 25
 
 ''' MAIN LOOP ------------------------------------------------------------------------------------ MAIN LOOP '''
@@ -145,16 +123,8 @@ while True:
         #Place the Gauge first (and any other fixed shapes)
         RECT = Gauge.get_rect()        
         DS.blit(Gauge, (DW_HALF - RECT.center[0] - 230, DH_HALF - RECT.center[1]))
-        pygame.draw.rect(DS, WHITE, (DW_HALF + 190, 173, 110, 30), 1)
-        if(FUELPERCVALUE > 60):
-                FUELCOLOR = RICHGREEN
-        elif(FUELPERCVALUE < 60):
-                if(FUELPERCVALUE > 30):
-                        FUELCOLOR = RICHYELLOW
-                else:
-                        FUELCOLOR = RICHRED
-        pygame.draw.rect(DS, FUELCOLOR, (DW_HALF + 195, 178, FUELX, 20), 0) 
-        pygame.draw.rect(DS, SILVER, (DW_HALF + 65, 235, 280, 220), 1)
+        pygame.draw.rect(DS, GREEN, (DW_HALF + 190, 173, 100, 30), 0)
+        pygame.draw.rect(DS, SILVER, (DW_HALF + 85, 255, 280, 220), 5)
 
         #Place all the fixed texts here
         display_text('RPM', DW_HALF+100, 50, 30)
