0f54c605f7449b9b7a7c8f3566159569348eb777
preetam shetty <preetamshetty22@gmail.com>
2019-09-27 19:11:57 +0530

    Update IC1.0_Display.py

--- Project/IC1.0_Display.py
+++ Project/IC1.0_Display.py
@@ -53,6 +53,7 @@ SELECTED = 1;
 MenuItemNumber = 1
 itemTextSize = 25
 itemSelectedSize = 35
+popupind = 0
 
 pygame.init()
 CLOCK = pygame.time.Clock() # *** NOT COVERED IN THE VIDEO ***
@@ -91,7 +92,7 @@ def display_logo():
         pygame.display.update()
         time.sleep(10)
         BootUp = False
-	
+
 def displayPopup():
         mainDisplay()
         DS.blit(popupBg, (380, 100))
@@ -100,7 +101,108 @@ def displayPopup():
         pygame.draw.rect(DS, WHITE, (380, 100, 240, 260), 2)
         DS.blit(popupIcon, (470, 190))
         pygame.display.update()
-        time.sleep(5)
+        time.sleep(5)		
+
+def mainDisplay():
+
+        global FUELCOLOR
+			#Place the Gauge first (and any other fixed shapes)
+        RECT = Gauge.get_rect()        
+        DS.blit(Gauge, (DW_HALF - RECT.center[0] - 230, DH_HALF - RECT.center[1]))
+        pygame.draw.rect(DS, WHITE, (DW_HALF + 190, 173, 110, 30), 1)
+        #pygame.draw.arc(DS, RED, [DW_HALF - 230, DH_HALF,20,20], degrees, 270) #(screen, color, (x,y,width,height), start_angle, stop_angle, thickness)
+        
+        if(FUELPERCVALUE > 60):
+                FUELCOLOR = RICHGREEN
+                print("Checkpoint green")
+        elif(FUELPERCVALUE < 60):
+                if(FUELPERCVALUE > 30):
+                        FUELCOLOR = RICHYELLOW
+                        print("Checkpoint yellow")
+                else:
+                        FUELCOLOR = RICHRED
+        pygame.draw.rect(DS, FUELCOLOR, (DW_HALF + 195, 178, FUELX, 20), 0) 
+        pygame.draw.rect(DS, SILVER, (DW_HALF + 65, 235, 280, 210), 1)
+
+        #Place all the fixed texts here
+        display_text('RPM', DW_HALF+100, 50, 30)
+        display_text('ODO', DW_HALF+280, 50, 30)
+        display_text('x1000', DW_HALF+100, 135, 20)
+        display_text('KM', DW_HALF+350, 135, 20)
+        display_text('FUEL', DW_HALF+100, 190, 40)
+        
+
+        #and then show the changed position of the needle
+        rotate_and_center(DS, DW_HALF - 230, DH_HALF, Needle, degrees)
+
+	#The degrees as of now is the global variable 
+        raw_value = float(((149 - degrees) * 28)/1000)
+
+        display_text(str(round(raw_value, 2)), DW_HALF+100, 100, 60)
+        display_text("123456", DW_HALF+270, 100, 50)
+
+        ListItemColor1 = GRAY
+        ListItemColor2 = GRAY
+        ListItemColor3 = GRAY
+        ListItemColor4 = GRAY
+        ListItemColor5 = GRAY
+        #ListItemColor6 = GRAY
+
+        if(MenuItemNumber == 1):
+                ListItemColor1 = WHITE
+
+        if(MenuItemNumber == 2):
+                ListItemColor2 = WHITE
+
+        if(MenuItemNumber == 3):
+                ListItemColor3 = WHITE
+
+        if(MenuItemNumber == 4):
+                ListItemColor4 = WHITE
+
+        if(MenuItemNumber == 5):
+                ListItemColor5 = WHITE
+
+        #if(MenuItemNumber == 6):
+                #ListItemColor6 = WHITE
+
+        if MenuNum <= 1:
+                pygame.draw.rect(DS, MENULISTCOLOR, (MenuStart_X - 14, MenuStart_Y + (Y_MenuSpace - 1)* (MenuItemNumber - 1), 278, 24), 0)
+                             
+                display_text_NC("**Settings**", MenuStart_X, MenuStart_Y - 40, 30, WHITE)
+
+                display_text_NC("Date and time", MenuStart_X, MenuStart_Y, 25, ListItemColor1)
+                display_text_NC("Avg Consumption", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 1), 25, ListItemColor2)
+                display_text_NC("Speed Govern", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 2), 25, ListItemColor3)
+                display_text_NC("Connectivity", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 3), 25, ListItemColor4)
+                display_text_NC("Infotainment", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 4), 25, ListItemColor5)
+                #display_text_NC("Menu item Six", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 5), 25, ListItemColor6)
+                
+                pygame.display.update(DW_HALF + 65, 235, 280, 210)
+
+        if(MenuNum >= 2):
+                pygame.draw.rect(DS, MENULISTCOLOR, (MenuStart_X - 14, MenuStart_Y + (Y_MenuSpace - 1)* (MenuItemNumber - 1), 278, 24), 0)
+                     
+                display_text_NC("**Maintenance**", MenuStart_X, MenuStart_Y - 40, 30, WHITE)
+
+                display_text_NC("Engine oil", MenuStart_X, MenuStart_Y, 25, ListItemColor1)
+                display_text_NC("Battery Check", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 1), 25, ListItemColor2)
+                display_text_NC("Range Check", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 2), 25, ListItemColor3)
+                display_text_NC("Notifications", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 3), 25, ListItemColor4)
+                display_text_NC("Calibration", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 4), 25, ListItemColor5)
+                #display_text_NC("Menu item Six", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 5), 25, ListItemColor6)
+
+                pygame.display.update(DW_HALF + 65, 235, 280, 210)
+
+        if(popupind > 0):
+                popupsurf = pygame.Surface(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+                popupsurf.fill((255,255,255))
+                popupsurf.set_alpha(128)
+                DS.blit(s, (0,0))
+        
+
+        #The tick in the bracket will tell the maximum frames per second. Lower number means slower speed. Its not a delay func
+        CLOCK.tick(60)
 
 def event_handler():
         global degrees
@@ -108,11 +210,14 @@ def event_handler():
         global FUELPERCVALUE
         global MenuNum
         global MenuItemNumber
+        global popupind
         
         for event in pygame.event.get():
                 if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-                        pygame.quit()
-                        sys.exit()
+                        #pygame.quit()
+                        #sys.exit()
+                        #mainDisplay()
+                        print("Quit")
                         
                 if event.type == QUIT or (event.type == KEYDOWN and event.key == K_a):
                         #global degrees
@@ -146,15 +251,27 @@ def event_handler():
                         if(MenuNum < 1):
                                 MenuNum = 2
 
-                if (event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN and MenuItemNumber < 6):
+                if (event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN and MenuItemNumber < 5):
                         MenuItemNumber += 1
                         
                 if (event.type == pygame.KEYDOWN and event.key == pygame.K_UP and MenuItemNumber > 1):
                         MenuItemNumber -= 1
-			
-		if event.type==pygame.KEYDOWN and event.key==pygame.K_1:
+
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_z):
+                        popupind = 1
+
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_x):
+                        popupind = 2
+                        
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_c):
+                        popupind = 3
+                        
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_v):
+                        popupind = 4
+		
+                if event.type==pygame.KEYDOWN and event.key==pygame.K_1:
                         displayPopup()
-                                
+                        
 def rotate_and_center(ds, x, y, image, degrees):
 # this function rotates an image and then centralises so that the rotation is uniform
 
@@ -197,97 +314,6 @@ def text_format(message, textFont, textSize, textColor):
     newText=newFont.render(message, 0, textColor)
 
     return newText
-
-def mainDisplay():
-     #Place the Gauge first (and any other fixed shapes)
-        RECT = Gauge.get_rect()        
-        DS.blit(Gauge, (DW_HALF - RECT.center[0] - 230, DH_HALF - RECT.center[1]))
-        pygame.draw.rect(DS, WHITE, (DW_HALF + 190, 173, 110, 30), 1)
-        #pygame.draw.arc(DS, RED, [DW_HALF - 230, DH_HALF,20,20], degrees, 270) #(screen, color, (x,y,width,height), start_angle, stop_angle, thickness)
-        
-
-        if(FUELPERCVALUE > 60):
-                FUELCOLOR = RICHGREEN
-        elif(FUELPERCVALUE < 60):
-                if(FUELPERCVALUE > 30):
-                        FUELCOLOR = RICHYELLOW
-                else:
-                        FUELCOLOR = RICHRED
-        pygame.draw.rect(DS, FUELCOLOR, (DW_HALF + 195, 178, FUELX, 20), 0) 
-        pygame.draw.rect(DS, SILVER, (DW_HALF + 65, 235, 280, 210), 1)
-
-        #Place all the fixed texts here
-        display_text('RPM', DW_HALF+100, 50, 30)
-        display_text('ODO', DW_HALF+280, 50, 30)
-        display_text('x1000', DW_HALF+100, 135, 20)
-        display_text('KM', DW_HALF+350, 135, 20)
-        display_text('FUEL', DW_HALF+100, 190, 40)
-        
-
-        #and then show the changed position of the needle
-        rotate_and_center(DS, DW_HALF - 230, DH_HALF, Needle, degrees)
-
-	#The degrees as of now is the global variable 
-        raw_value = float(((149 - degrees) * 28)/1000)
-
-        display_text(str(round(raw_value, 2)), DW_HALF+100, 100, 60)
-        display_text("123456", DW_HALF+270, 100, 50)
-
-        ListItemColor1 = GRAY
-        ListItemColor2 = GRAY
-        ListItemColor3 = GRAY
-        ListItemColor4 = GRAY
-        ListItemColor5 = GRAY
-        ListItemColor6 = GRAY
-
-        if(MenuItemNumber == 1):
-                ListItemColor1 = WHITE
-
-        if(MenuItemNumber == 2):
-                ListItemColor2 = WHITE
-
-        if(MenuItemNumber == 3):
-                ListItemColor3 = WHITE
-
-        if(MenuItemNumber == 4):
-                ListItemColor4 = WHITE
-
-        if(MenuItemNumber == 5):
-                ListItemColor5 = WHITE
-
-        if(MenuItemNumber == 6):
-                ListItemColor6 = WHITE
-
-        if MenuNum <= 1:
-                pygame.draw.rect(DS, MENULISTCOLOR, (MenuStart_X - 14, MenuStart_Y + (Y_MenuSpace - 1)* (MenuItemNumber - 1), 278, 24), 0)
-                             
-                display_text_NC("< Settings >", MenuStart_X, MenuStart_Y - 40, 30, WHITE)
-
-                display_text_NC("Date and time", MenuStart_X, MenuStart_Y, 25, ListItemColor1)
-                display_text_NC("Notifications", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 1), 25, ListItemColor2)
-                display_text_NC("Calibration", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 2), 25, ListItemColor3)
-                display_text_NC("Connectivity", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 3), 25, ListItemColor4)
-                display_text_NC("Menu item Five", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 4), 25, ListItemColor5)
-                display_text_NC("Menu item Six", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 5), 25, ListItemColor6)
-                
-                pygame.display.update(DW_HALF + 65, 235, 280, 210)
-
-        if(MenuNum >= 2):
-                pygame.draw.rect(DS, MENULISTCOLOR, (MenuStart_X - 14, MenuStart_Y + (Y_MenuSpace - 1)* (MenuItemNumber - 1), 278, 24), 0)
-                     
-                display_text_NC("< Maintenance >", MenuStart_X, MenuStart_Y - 40, 30, WHITE)
-
-                display_text_NC("Battery", MenuStart_X, MenuStart_Y, 25, ListItemColor1)
-                display_text_NC("Starter battery", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 1), 25, ListItemColor2)
-                display_text_NC("Air filter", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 2), 25, ListItemColor3)
-                display_text_NC("Engine oil", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 3), 25, ListItemColor4)
-                display_text_NC("Menu item Five", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 4), 25, ListItemColor5)
-                display_text_NC("Menu item Six", MenuStart_X, (MenuStart_Y + Y_MenuSpace * 5), 25, ListItemColor6)
-
-                pygame.display.update(DW_HALF + 65, 235, 280, 210)
-
-        #The tick in the bracket will tell the maximum frames per second. Lower number means slower speed. Its not a delay func
-        CLOCK.tick(60)
 	
 ''' SETUP VARIABLES ------------------------------------------------------------------------------ SETUP VARIABLES '''
 # set the starting rotation of the image in degrees 
@@ -295,7 +321,8 @@ direction = 1;
 raw_value = degrees * 0.8
 MenuStart_X = DW_HALF + 80
 MenuStart_Y = 280
-Y_MenuSpace = 25
+#Y_MenuSpace = 25
+Y_MenuSpace = 35
 
 ''' MAIN LOOP ------------------------------------------------------------------------------------ MAIN LOOP '''
 while True:
@@ -305,8 +332,8 @@ while True:
                                 
         #Event handling currently handles ESC or any other keypress. This is a polling mechanism
         event_handler()
-
-        mainDisplay()
+		
+        mainDisplay()        
 			
         pygame.display.update() #pygame.display.flip() is another option
         DS.fill([0,0,0])
